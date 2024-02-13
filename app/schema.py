c4ea8b538dd38d6b39b2ca21a8bc3ac1d488a397
Viktor Lavron <lavron@lavron.info>
2024-02-13 16:34:53 +0200

    Update schema

--- app/schema.py
+++ app/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
-from app.models import Game, Player, Sector, ProductCard, EventCard
+from app.models import Game, Player, Sector, ProductCard, EventCard, Trader
 
 
 class GameType(DjangoObjectType):
@@ -28,12 +28,18 @@ class EventCardType(DjangoObjectType):
         model = EventCard
 
 
+class TraderCardType(DjangoObjectType):
+    class Meta:
+        model = Trader
+
+
 class Query(graphene.ObjectType):
     games = graphene.List(GameType)
     players = graphene.List(PlayerType)
     sectors = graphene.List(SectorType)
     product_cards = graphene.List(ProductCardType)
     event_cards = graphene.List(EventCardType)
+    traders = graphene.List(TraderCardType)
 
     def resolve_game(self, info, pk, **kwargs):
         return Game.objects.get(pk=pk)
@@ -52,6 +58,9 @@ class Query(graphene.ObjectType):
 
     def resolve_event_cards(self, info, **kwargs):
         return EventCard.objects.all()
+
+    def resolve_traderss(self, info, **kwargs):
+        return Trader.objects.all()
 
 
 schema = graphene.Schema(query=Query)
