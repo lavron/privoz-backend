00670fa8496b768992b500f15762d48cf58a98c2
Viktor Lavron <lavron@lavron.info>
2024-02-13 10:36:32 +0200

    new fields at default endpoint

--- app/views.py
+++ app/views.py
@@ -9,21 +9,31 @@ def game_box(request, game_id):
     product_cards = ProductCard.objects.filter(games=game)
     event_cards = EventCard.objects.filter(games=game)
 
-
     game_data = {
         'pk': game.pk,
         # add other game fields here
     }
-    players_data = [{'pk': player.pk, 'name': player.name, 'coins': player.coins, 'color': player.color} for player in
-                    players]
-    sectors_data = [{'pk': sector.pk, 'name': sector.name, 'description': sector.description} for sector in sectors]
+    players_data = [{
+        'pk': player.pk,
+        'name': player.name,
+        'coins': player.coins,
+        'color': player.color,
+        'premium_sector': player.premium_sector.pk,
+        'event_card_protection': player.event_card_protection.pk
+
+    } for player in players]
+    sectors_data = [{
+        'pk': sector.pk,
+        'name': sector.name,
+        'description': sector.description} for sector in sectors]
     product_cards_data = [{
         'pk': card.pk,
         'name': card.name,
         'description': card.description,
+        'quantity': card.quantity,
         'image': card.image,
         'is_legal': card.is_legal,
-        'sector': {'pk': card.sector.pk, 'name': card.sector.name, 'description': card.sector.description},
+        'sector': card.sector.pk,
         'sell_price': card.sell_price,
         'buy_price': card.buy_price,
 
@@ -34,6 +44,7 @@ def game_box(request, game_id):
         'name': card.name,
         'description': card.description,
         'location': card.location,
+        'quantity': card.quantity,
         'fortune': card.fortune,
         'target': card.target,
         'confiscation': card.confiscation,
@@ -41,6 +52,8 @@ def game_box(request, game_id):
         'player_extra_profit': card.player_extra_profit,
         'trader_extra_profit': card.trader_extra_profit,
         'product_extra_profit': card.product_extra_profit,
+        'product_extra_item': card.product_extra_item,
+
         # 'trader_is_active': card.trader_is_active,
 
     } for card in event_cards]
@@ -54,5 +67,3 @@ def game_box(request, game_id):
     }
 
     return JsonResponse(data, safe=False)
-
-
