1b8651be3eb12c3f1f3f6cf2dd69e0fd2b3d7625
Viktor Lavron <lavron@lavron.info>
2024-02-13 17:21:10 +0200

    fix Trader model

--- app/models/player.py
+++ app/models/player.py
@@ -9,8 +9,8 @@ class Player(models.Model):
     event_card_protection = models.ForeignKey('EventCard', related_name='event_card_protection',
                                               on_delete=models.CASCADE, null=True)
     traders = models.ForeignKey('Trader', related_name='player', on_delete=models.CASCADE, null=True, default=None)
-    event_cards = models.ManyToManyField('EventCard', related_name='players', default=None)
-    product_cards = models.ManyToManyField('ProductCard', related_name='players', default=None)
+    event_cards = models.ManyToManyField('EventCard', related_name='players', default=None, blank=True)
+    product_cards = models.ManyToManyField('ProductCard', related_name='players', default=None, blank=True)
 
     def __str__(self):
         return self.name
