a25b696c03bfb736d1a80ca90a5a1342c0d73294
Viktor Lavron <lavron@lavron.info>
2024-02-14 08:04:51 +0200

    fix CORS headers

--- privoz/utils.py
+++ privoz/utils.py
@@ -3,7 +3,7 @@ class DisableCSRFCheck:
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.path == '/your_endpoint/':  # replace 'your_endpoint' with your actual endpoint
+        if request.path == '/graphql/':  # replace 'your_endpoint' with your actual endpoint
             setattr(request, '_dont_enforce_csrf_checks', True)
         response = self.get_response(request)
         return response
