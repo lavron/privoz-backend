6e3cc090da2be4e8b8ce322739b5b4a009a4db17
Viktor Lavron <lavron@lavron.info>
2024-02-19 16:01:21 +0200

    check user's turn

--- app/schema/mutations/hire_trader.py
+++ app/schema/mutations/hire_trader.py
@@ -16,6 +16,12 @@ class HireTrader(graphene.Mutation):
     @staticmethod
     def mutate(root, info, player_id, sector_id, products_ids):
         player = Player.objects.get(id=player_id)
+
+        game = player.game
+        current_player_id = game.turn_order[game.current_turn_index]
+        if player.id != current_player_id:
+            raise Exception('It is not this player\'s turn')
+
         trader = TraderService.hire(player_id, sector_id, products_ids)
         player.game.end_turn()
         return HireTrader(trader=trader)
